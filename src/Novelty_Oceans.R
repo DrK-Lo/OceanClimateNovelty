fc8c978ca97bac23c98142fbd6547a929e5a6dc6
Katie Lotterhos <k.lotterhos@gmail.com>
2018-10-30 19:41:57 -0400

    more edits

--- src/Novelty_Oceans.R
+++ src/Novelty_Oceans.R
@@ -14,6 +14,7 @@ library(adehabitatLT)   #provides the chi distribution
 library(data.table)
 library(tidyverse)
 library(ggplot2)
+library(fields)
 ##################
 #### Read in the input data 
 ##################
@@ -119,6 +120,7 @@ head(norm_2000)
 
 #--------------------------------  
 ### Detrend for ICV and get sd ####
+### Note I don't think this is a conservative way to do this
 #--------------------------------
 
 get_detrend <- function(dat1){
@@ -147,9 +149,62 @@ get_detrend <- function(dat1){
                      "SST_win", "Arag_win", "Calc_win")
 }
 
-sd_stations <- get_detrend(dat_2000)
+
+#--------------------------------  
+### Get annual ICV
+#--------------------------------
+
+get_ICV <- function(dat1){
+  get_sd_resids <- function(i){
+    # dat1 <- dat_2000
+    # plot(dat1$SST[dat1$No==i]~dat1$Month[dat1$No==i])
+    SST_sd <- sd(dat1$SST[dat1$No==i])
+    Arag_sd <- sd(dat1$Arag[dat1$No==i])
+    Calc_sd <- sd(dat1$Calc[dat1$No==i])
+    #points(dat1$Month[dat1$No==i],predict(mod), pch=19)
+    return(c(SST_sd, Arag_sd, Calc_sd))
+  }
+  stations <- sort(unique(dat1$No))
+  sds<- sapply(stations, get_sd_resids)
+  # this takes ~10 minutes to run
+  out <- data.frame(stations, t(sds), t(sds))
+  colnames(out) <- c("No", "SST_sum", "Arag_sum", "Calc_sum",
+                     "SST_win", "Arag_win", "Calc_win")
+  return(out)
+}
+
+sd_stations <- get_ICV(dat_2000)
   # this takes about 10 min to run
- 
+head(sd_stations) 
+
+#--------------------------------  
+### data frame to link station number to Lat Long ####
+#--------------------------------
+head(dat)
+min(which(dat$No==2))
+stations <- unique(dat$No)
+stationInfo = data.frame(stations=stations, lat=NA, long=NA)
+unik <- which(!duplicated(dat$No))
+head(unik)
+stationInfo$lat <- dat$Lat[unik]
+stationInfo$long <- dat$Lon[unik]
+
+identical(stationInfo$stations, sd_stations$No)
+stationInfo <- data.frame(stationInfo, sd_stations)
+
+head(stationInfo)
+which(!complete.cases(stationInfo))
+
+### 
+image.plot(x=stationInfo$lat, y=stationInfo$long, z=stationInfo$SST_sum)
+
+ggplot(stationInfo) + geom_hex(aes(x = long, 
+                                   y = lat,
+                                   fill = SST_sum),
+                               binwidth = c(5, 5))+
+  scale_fill_gradientn(colours = rainbow(7))
+
+
 #--------------------------------  
 ### 1800 analog to today ####
 #--------------------------------
